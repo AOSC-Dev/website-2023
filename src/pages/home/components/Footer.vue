391b2d1c6a91c95312e03bdaf49b21241708a597
shatian114 <1320024819@qq.com>
2024-04-08 20:20:30 +0800

    fix: 修复上部背景图自适应问题 refactor: 调整左右留白为15%

--- src/pages/home/components/Footer.vue
+++ src/pages/home/components/Footer.vue
@@ -5,7 +5,7 @@ const nowYear = new Date().getFullYear();
 
 <template>
   <div class="bg-secondary">
-    <div class="text-white py-auto px-[10vw]">
+    <div class="text-white py-auto px-[15vw]">
       <span>&copy; 安同开源社区 2011 - {{ nowYear }}</span>
       <span class="mx-[20px]">|</span>
       <a href="">网页源码</a>
