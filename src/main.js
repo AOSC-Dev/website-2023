4c48208acfb8d130c0dd9f927a7432f1786351c1
shatian114 <1320024819@qq.com>
2024-05-27 14:28:42 +0800

    feat: 主页左边菜单新增折叠功能

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import '@kangc/v-md-editor/lib/style/preview.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
 import './css/index.scss';
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { BiChevronDoubleUp, BiChevronDoubleDown } from "oh-vue-icons/icons";
+
+addIcons(BiChevronDoubleUp, BiChevronDoubleDown);
 
 // highlightjs
 import hljs from 'highlight.js';
@@ -17,6 +21,7 @@ VMdPreview.use(githubTheme, {
 });
 
 const app = createApp(App)
+app.component("v-icon", OhVueIcon);
 app.use(router)
 app.use(VMdPreview)
 app.mount('#app')
