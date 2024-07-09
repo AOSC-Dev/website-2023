eed8d15e53d267b3c64d7f3f65a1aa6888dc6e16
eatradish <sakiiily@aosc.io>
2024-07-09 13:58:40 +0800

    ci: 改良 CI

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 rm -rf website*
+
+# Generate news
+pushd ./makeCategory
+./build.sh
+popd
+pushd ./public
+./makeCategory
+popd
+
+# Build website
+pnpm i
 pnpm run build
 zip -r website.zip ../website/
 scp website.zip aosc@20.115.186.5:/home/aosc/web/
