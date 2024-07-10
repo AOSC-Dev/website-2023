6c4196fdb44481e63623c47b41c8b9e14a41395f
shatian114 <1320024819@qq.com>
2024-07-10 09:56:51 +0800

    refactor: 优化社区吉祥物页面布局

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -8,27 +8,16 @@ const props = defineProps({
     type: String,
     default: "",
   },
-  width: {
-    type: Number,
-    default: 100,
-  },
-  height: {
-    type: Number,
-    default: 50,
-  },
 });
 </script>
 
 <template>
-  <div>
-    <a
-      target="_blank"
-      :href="link"
-      class="flex justify-center items-center bg-primary text-white block hover:no-underline"
-      :style="{ width: width + 'px', height: height + 'px' }"
-      >{{ text }}</a
-    >
-  </div>
+  <a
+    target="_blank"
+    :href="link"
+    class="flex justify-center items-center bg-primary text-white block hover:no-underline"
+    >{{ text }}</a
+  >
 </template>
 
 <style scoped></style>
