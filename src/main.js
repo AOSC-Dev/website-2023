5f0651ae84e56cdc2104cdb56a52c090f0ad3408
shatian114 <1320024819@qq.com>
2024-07-17 22:32:46 +0800

    fix: 修复element-plus样式未导入

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import { install as VueMonacoEditorPlugin } from '@guolao/vue-monaco-editor'
@@ -20,8 +21,6 @@ import Prism from 'prismjs';
 VMdPreview.use(vuepressTheme, {
   Prism,
 });
-
-
 
 // highlightjs
 import 'highlight.js/styles/stackoverflow-light.css'
