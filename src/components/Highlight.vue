2639a534894d864e433ab58a34023fb8c950e16e
shatian114 <1320024819@qq.com>
2024-07-18 11:06:55 +0800

    refactor: 修改代码块复制标识为icon

--- src/components/Highlight.vue
+++ src/components/Highlight.vue
@@ -37,7 +37,7 @@ function copy() {
     <pre class="relative">
     <code :class="`hljs ${props.lang}`" ref="codeRef">
     </code>
-    <div class="absolute top-[2em] right-[1em] cursor-pointer text-link" @click="copy">复制</div>
+    <v-icon class="text-primary absolute top-[2em] right-[1em] cursor-pointer" name="fa-copy" @click="copy" />
   </pre>
     
 </template>
