314a8ba276da09fa2e655f3e8bd1cd8fd5f23d13
Cyan <cyanoxygen@aosc.io>
2024-07-23 16:54:23 +0800

    download: 修正镜像版本排序；区分安装器和 LiveKit（救援环境）

--- src/pages/download/components/DownloadButton.vue
+++ src/pages/download/components/DownloadButton.vue
@@ -1,11 +1,12 @@
 <script setup DownloadButton>
-const props = defineProps(["isaInfo"]);
+import { onMounted, ref } from "vue";
 
+const props = defineProps(["labelInfo", "isaInfo"]);
 function byteToGb(bytes) {
   return (bytes / 1024 / 1024 / 1024).toFixed(2);
 }
 function downloadIso() {
-  window.open(`https://releases.aosc.io/${props.isaInfo.info.path}`);
+  window.open(`https://releases.aosc.io/${props.isaInfo.path}`);
 }
 </script>
 <template>
@@ -13,8 +14,8 @@ function downloadIso() {
     class="w-[224px] m-0 text-white bg-secondary hover:opacity-85 ml-1 cursor-pointer text-[10pt]"
     @click="downloadIso"
   >
-    <div>{{ props.isaInfo.zhLabel }}</div>
-    <p>{{ byteToGb(props.isaInfo.info.downloadSize) }}GB ISO</p>
+    <div>{{ props.labelInfo.zhLabel }}</div>
+    <p>{{ byteToGb(props.isaInfo.downloadSize) }}GB ISO</p>
   </button>
 </template>
 
