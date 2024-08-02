841803711d3cdf1cbb5a96650bf143c770382e3a
hrh <hrhdeyouxiang@qq.com>
2024-08-02 19:01:44 +0800

    download: 调整页面样式，安同OS下载链接所属块在足够宽时将开启双列展示; 缩小了LiveKit块和子块的大小。

--- src/pages/download/components/DownloadButton.vue
+++ src/pages/download/components/DownloadButton.vue
@@ -1,28 +1,43 @@
 <script setup DownloadButton>
 import { onMounted, ref } from "vue";
 
-const props = defineProps(["labelInfo", "isaInfo"]);
+const props = defineProps({
+  labelInfo: {
+    type:Object
+  },isaInfo: {
+    type:Object
+  },secondLineFontSize: {  
+    type: Number,  
+    default: 10  
+  },firstLineFontSize: {
+    type:Number,
+    default: 12
+  },width: {
+    type:Number,
+    default: 224
+  }})
 function byteToGb(bytes) {
   return (bytes / 1024 / 1024 / 1024).toFixed(2);
 }
+console.log(props.labelInfo)
 function downloadIso() {
   window.open(`https://releases.aosc.io/${props.isaInfo.path}`);
 }
 </script>
 <template>
   <button
-    class="w-[224px] m-0 text-white bg-secondary hover:opacity-85 ml-1 cursor-pointer text-[10pt]"
+    class="text-white bg-secondary hover:opacity-85 cursor-pointer mx-1"
+    :style="{width:$props.width + 'px'}"
     @click="downloadIso"
   >
-    <div>{{ props.labelInfo.zhLabel }}</div>
-    <p>{{ byteToGb(props.isaInfo.downloadSize) }}GB ISO</p>
+    <div :style="{fontSize: $props.firstLineFontSize+'pt'}">{{ props.labelInfo.zhLabel }}</div>
+    <p :style="{fontSize: $props.secondLineFontSize+'pt'}">{{ byteToGb(props.isaInfo.downloadSize) }}GB ISO</p>
   </button>
 </template>
 
 <style scoped>
 .button {
   border: #7f9e7f;
-  border-radius: 5px;
 }
 
 .button-p {
