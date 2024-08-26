0bf1dda9634581bb41abdec253047134eb421d0c
hrh <hrhdeyouxiang@qq.com>
2024-08-26 18:22:23 +0800

    refactor: 重构链接与导航逻辑
    - 添加AppLink组件，超链接列表混合本地/外部链接的情况不必再使用 v-if 属性
    - 更新链接及按钮高亮实现
    - path 不变时的导航高亮由路由守卫刷新 store 变量，watch 监控 store 变量触发
    - path 变化时的导航高亮由onMount触发

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 import router from '../router.js'
+import axios from 'axios'
 
 export function getImgUrl(url) {
   return new URL(`${url}`, import.meta.url).href
@@ -21,3 +22,30 @@ export function toUrl(url) {
     router.push(url);
   }
 }
+
+export const requestJson = (() => {
+  let keys = {}
+  let promise={}
+  return (url, parameterObj, key) => {
+    if (!key) {
+      key = url;
+    }
+    if (!keys[key]) {
+      keys[key] = true;
+      promise[key] = axios({
+        url,
+        method: 'get',
+        data: parameterObj
+      }).then(resolve => {
+        return [resolve.data, null];
+      }).catch(error => {
+        [null, error];
+      }).finally(() => {
+        keys[key] = false;
+      })
+      return promise[key]
+    } else {
+      return promise[key];
+    }
+  }
+})()
