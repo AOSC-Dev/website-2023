f6fd12b0164fbbf905b18680179a965dda2b2281
hrh <hrhdeyouxiang@qq.com>
2024-09-18 17:37:51 +0800

    news/List.Vue 更名为 news/NewsList.vue 修复了此文件上一个 commit 误删除常量定义导致的 bug

---
+++ src/pages/news/NewsList.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+import CategorySecond from '/src/components/CategorySecond.vue';
+import NewsCategoryList from './components/NewsCategoryList.vue';
+import { useRoute } from 'vue-router';
+import { requestGetJson } from '../../utils/utils';
+
+const categories = ref({});
+const categoryTitle = ref('');
+const route = useRoute();
+
+(async () => {
+  const category = route.params.category;
+  categoryTitle.value =
+    route.params.categoryTitle;
+  let [res, err] = await requestGetJson(
+    `/newsCategories/${category}.zh-cn.json`
+  );
+  if (res) {
+    console.log('新闻目录', res.data);
+    categories.value = res.data;
+  } else if (err) {
+    console.log(err);
+  }
+})();
+</script>
+
+<template>
+  <div class="pl-[1px]">
+    <category-second
+      :title="categoryTitle"
+      class="border-r-solid border-r-white" />
+    <news-category-list
+      :news-list="categories || []" />
+  </div>
+</template>
+
+<style scoped></style>
+./components/Header.vue
