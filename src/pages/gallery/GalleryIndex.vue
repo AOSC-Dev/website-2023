0f3732a95479fd4753a558e51c021b8f93e01c38
shatian114 <1320024819@qq.com>
2024-09-22 14:47:28 +0800

    refactor: 修改相册的图片路径为website服务器的路径

--- src/pages/gallery/GalleryIndex.vue
+++ src/pages/gallery/GalleryIndex.vue
@@ -47,7 +47,7 @@ function closeBigImg() {
           :key="photo.file">
           <img
             loading="lazy"
-            :src="`https://aosc.io/assets/gallery/thumbs/${photo.file}.jpg`"
+            :src="`/gallery/thumbs/${photo.file}.jpg`"
             :alt="photo.desc"
             class="cursor-pointer w-[100%] max-w-[100%] object-cover aspect-video"
             @click="clickImg(gallery, index)" />
@@ -73,7 +73,7 @@ function closeBigImg() {
             class="flex justify-center w-[100%] h-[100%]">
             <img
               class="w-[100%] h-[auto] object-contain"
-              :src="`https://aosc.io/assets/gallery/${photo.file}`"
+              :src="`/gallery/${photo.file}`"
               :alt="photo.desc" />
           </div>
         </el-carousel-item>
