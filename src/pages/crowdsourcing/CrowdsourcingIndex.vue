72d13f49192054d2ca592a50d2d02b21ad22d155
Mingcong Bai <jeffbai@aosc.io>
2024-09-23 21:30:05 +0800

    fix(crowdsourcing): 统一标题

--- src/pages/crowdsourcing/CrowdsourcingIndex.vue
+++ src/pages/crowdsourcing/CrowdsourcingIndex.vue
@@ -4,7 +4,7 @@ import CategorySecond from '/src/components/CategorySecond.vue';
 
 <template>
   <div class="pl-[1px]">
-    <category-second title="众筹" />
+    <category-second title="社区众筹" />
     <div class="p-6">
       <p>根据社区<router-link to="/guidelines" class="text-link">《人际关系准则》</router-link>的要求，社区不接受作为社区及项目运转资金之用的捐款。但由于我社作为无运作资本的志愿者社区，在社区日常运营及为具体项目购置大宗物件（如构建用服务器和 AOSCC 年度聚会等场合）时经常需要资金及物质支持。因此，我们接受针对特定项目及购买计划的金钱、物品（如电脑机箱、电源及硬盘等）及服务（如网站服务器资源等）捐赠。</p><br />
       <p>您可以前往 Wiki 站点查阅<a class="text-link" href="https://wiki.aosc.io/community/crowdsourcing/">众筹项目记录</a>。</p>
