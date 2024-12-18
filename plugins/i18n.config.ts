HouLiXieBuRou/website-nuxt

Temporary storage work

plugins/i18n.config.ts
@@ -1,0 +1,11 @@
+import zhCN from '~/locales/zh-CN.json';
+import enUS from '~/locales/en-US.json';
+export default defineI18nConfig(() => ({
+  legacy: false,
+  fallbackLocale: 'zh-cn',
+  locale: 'zh-cn',
+  messages: {
+    'en-us': enUS,
+    'zh-cn': zhCN
+  }
+}));

AOSC-Dev/website-2023

plugins/i18n.config.ts
@@ -1,0 +1,11 @@
+import zhCNData from '~/locales/zh-CN.js';
+import enUSData from '~/locales/en-US.js';
+export default defineI18nConfig(() => ({
+  legacy: false,
+  fallbackLocale: 'zh-cn',
+  locale: 'zh-cn',
+  messages: {
+    'en-us': enUSData,
+    'zh-cn': zhCNData
+  }
+}));