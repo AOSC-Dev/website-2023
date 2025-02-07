2b91f7fa5fec6cc708bc8b8bbd50a2c816db1b95
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-02-07 23:34:29 +0800

    feat(download): add instruction page for Apple silicon

---
+++ src/pages/download/components/CopyCodeComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ElMessage } from 'element-plus';
+import useClipboard from 'vue-clipboard3';
+const { toClipboard } = useClipboard();
+
+const props = defineProps({
+  content: { type: String },
+  buttonText: { type: String, optional: true, default: '复制' }
+});
+
+function copy(content) {
+  toClipboard(content);
+  ElMessage.success('复制成功');
+}
+</script>
+
+<template>
+  <el-container>
+    <code
+      class="flex-grow overflow-x-auto text-nowrap p-[5px_10px] shadow-[inset_0_0_0_1px_var(--el-border-color)]">
+      {{ content }}
+    </code>
+    <el-button @click="copy(content)" plain color="gray">
+      {{ buttonText }}
+    </el-button>
+  </el-container>
+</template>
