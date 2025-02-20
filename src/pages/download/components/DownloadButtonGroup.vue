424eb7305e28af0b3f87771e2f97cf7ffd45a362
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-02-20 15:44:49 +0800

    feat(download): add animations for button groups

--- src/pages/download/components/DownloadButtonGroup.vue
+++ src/pages/download/components/DownloadButtonGroup.vue
@@ -26,14 +26,13 @@ const expand = ref(false);
     <div
       v-if="buttonProps.length > 3"
       @click="expand = !expand"
-      class="flex justify-center items-center theme-bg-color-secondary-primary text-[11pt] py-1 mb-1">
+      class="flex justify-center items-center theme-bg-color-secondary-primary text-[11pt] py-1 mb-1 cursor-pointer">
       <span>{{ expand ? '收起架构' : '展开架构' }}</span>
       <v-icon name="md-arrowdropdown" :class="expand ? 'rotate-180' : ''" />
     </div>
 
     <div
-      v-if="buttonProps.length <= 3 || expand"
-      class="grid grid-cols-[repeat(3,1fr)] auto-rows-fr gap-x-4 gap-y-2">
+      :class="`buttons grid grid-cols-[repeat(3,1fr)] auto-rows-fr gap-x-4 gap-y-2 ${buttonProps.length <= 3 || expand ? 'expand':''}`">
       <DownloadButton
         v-for="buttonProp in buttonProps"
         :key="buttonProp.archName"
@@ -43,4 +42,16 @@ const expand = ref(false);
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.buttons {
+  overflow: hidden;
+  max-height: 0;
+  opacity: 0.5;
+  transition: all 0.3s;
+}
+
+.buttons.expand {
+  opacity: 1;
+  max-height: 1000px;
+}
+</style>
