e61e78a17e35d2f5d341249d9c31b6420873da64
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-02-24 16:25:30 +0800

    fix(footer): fix footer width on narrow viewports

--- src/pages/home/components/FooterBar.vue
+++ src/pages/home/components/FooterBar.vue
@@ -21,7 +21,7 @@ const navigationList = [
 
 <template>
   <div
-    class="theme-bg-color-secondary-static text-white py-[2px] flex justify-center w-[100vw] footerbar">
+    class="theme-bg-color-secondary-static text-white py-[2px] flex justify-center w-full footerbar">
     <div class="m-auto">
       <span class="pl-[0.5rem]"
         >&copy; 安同开源社区&nbsp;&nbsp;&nbsp;&nbsp;2011 - {{ nowYear }}</span
