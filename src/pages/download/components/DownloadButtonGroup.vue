e1000c2176eb73b69b2ff3254fb4a9dc2f77b87a
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-03-22 23:15:26 +0800

    chore: simplify code of previous commit

--- src/pages/download/components/DownloadButtonGroup.vue
+++ src/pages/download/components/DownloadButtonGroup.vue
@@ -12,25 +12,14 @@ const props = defineProps({
 const expand = ref(false);
 </script>
 
-<script>
-export default {
-  data() {
-    return { isVisible: false }
-  }
-}
-</script>
-
 <template>
   <div class="flex flex-col gap-1">
     <div class="leading-none">
       <span v-if="title" class="text-[10pt] font-[450]">{{ title }}</span>
-      <el-popover 
-        placement="top"
-        width="233"
-        ref="whatsThisArchLevelPopup"
-        manual
-        v-model="isVisible">
-        <div @click="$refs.whatsThisArchLevelPopup.hide()">{{ description }}</div>
+      <el-popover placement="top" width="233" ref="whatsThisArchLevelPopup">
+        <div @click="$refs.whatsThisArchLevelPopup.hide()"
+          >{{ description }}
+        </div>
         <template #reference>
           <span class="text-[8pt] font-[450]">（这是什么？）</span>
         </template>
