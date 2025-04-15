1d14e3d5bc349d072e520f069d0a5b0ac2802d42
Mingcong Bai <jeffbai@aosc.io>
2025-04-15 17:46:53 +0800

    feat(aosc-os/livekit/relnote): 更新 2025 年 4 月发行注记

--- src/pages/aosc-os/livekit/relnote/AoscLivekitRelnote.vue
+++ src/pages/aosc-os/livekit/relnote/AoscLivekitRelnote.vue
@@ -4,39 +4,26 @@ import CategorySecond from '/src/components/CategorySecond.vue';
 
 <template>
   <div class="pl-[1px]">
-    <category-second title="发行注记：2024 年 9 月" />
+    <category-second title="发行注记：2025 年 4 月" />
     <div class="p-6">
-      <p>
-        LiveKit 是用于维护安同 OS
-        和进行故障修复的可移动环境，本次发行包含的新特性与更改如下。 </p
-      ><br />
+      <p>LiveKit 是用于维护安同 OS 和进行故障修复的可移动环境，本次发行包含的新特性与更改如下。 </p><br />
     </div>
 
     <category-second title="新特性与更改" id="features" />
     <div class="p-6">
-      <p>除将系统组件更新到 2024 年 9 月的基线外，LiveKit 还包含如下新特性：</p
-      ><br />
+      <p>
+        除将系统组件更新到 2025 年 4 月的基线外，LiveKit 还包含如下新特性：
+      </p><br />
       <div class="px-16 pt-2 pb-4">
         <ul class="list-disc">
-          <li>更新 Linux 内核至 6.10 分支，支持更多新硬件</li>
-          <li>支持龙架构旧世界固件引导安同 OS 安装盘</li>
-          <li
-            >修复带有 Optimus（带有核显及 NVIDIA
-            独显的双显卡设备）功能的多显卡设备支持</li
-          >
-          <li>将安同 OS 安装程序（在线模式）更新至最新版，修复大量已知问题</li>
-          <li
-            >以基于新 DeployKit 后端编写的新命令行安装程序
-            [dkcli](https://github.com/AOSC-Dev/dkcli)
-            取代旧的命令行安装程序，可支持无人值守安装</li
-          >
-          <li
-            >为安装及试用环境禁用屏幕节电及自动睡眠等电源功能，避免产生意外故障</li
-          >
-          <li>采取措施避免生成安装盘时预生成 SSH 主机密钥</li>
-          <li>删除预设的 `live` 用户密码</li>
-          <li>将内存检测工具 Memtest86+ 升级至 7.00</li>
-          <li>新增救援与修复控制台（命令行安装环境）说明文字的简体中文翻译</li>
+          <li>更新 Linux 内核至 6.14.1，支持更多新硬件</li>
+          <li>改进下拉菜单操作逻辑，安装可在仅使用键盘的情况下完成</li>
+          <li>修复安装时未禁止使用系统预留用户名的问题</li>
+          <li>修复系统安装后未正确探测设备上其他操作系统的问题</li>
+          <li>修复安装过程中屏幕可能熄灭，导致用户不安的问题</li>
+          <li>新增安装程序日志记录功能</li>
+          <li>新增多语言启动菜单支持</li>
+          <li>新增使用 LEFI 固件的龙芯三号 (MIPS) 3A4000/3B4000 平台安装支持</li>
         </ul>
       </div>
     </div>
