16e96cce5ac132fc4decb2dba9d14ef0d9da560f
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-06-18 14:50:48 +0800

    feat(aoscc): update AosccIndex and AosccLinkButton components for improved routing and link handling

--- src/pages/aoscc/components/AosccLinkButton.vue
+++ src/pages/aoscc/components/AosccLinkButton.vue
@@ -1,6 +1,13 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import AppLink from '../../../components/AppLink.vue';
+const props=defineProps({...RouterLink.props});
+</script>
+
 <template>
-  <a
+  <AppLink
+    v-bind="props"
     class="theme-bg-color-secondary-primary cursor-pointer overflow-hidden px-4 py-2 hover:no-underline">
     <slot></slot>
-  </a>
+  </AppLink>
 </template>
