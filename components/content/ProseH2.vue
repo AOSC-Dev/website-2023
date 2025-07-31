7a21b158c693fba12d267d41e1aa4fba50e11c71
hrh <hrhdeyouxiang@qq.com>
2025-07-31 18:34:39 +0800

    fix: 修复新闻页面里 h2 h3 标签被错误的识别为链接

---
+++ components/content/ProseH2.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h2 class="heti">
+    <slot />
+  </h2>
+</template>
+
+<style scoped></style>
---
+++ components/content/ProseH3.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h3 class="heti">
+    <slot />
+  </h3>
+</template>
+
+<style scoped></style>
