ac59641ca22dc4410e4547d30efeb0ffafd160fe
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-08-02 00:11:56 +0800

    fix: incorrect build-cf.sh

--- deploy/build-cf.sh
+++ deploy/build-cf.sh
@@ -4,9 +4,6 @@ if [ "$CF_PAGES_BRANCH" == "website-nuxt" ]; then
     npm run generate
     ln -s .output/public website
 
-elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
-    npm run build
-
 else
-    npm run dev
+    npm run build
 fi
