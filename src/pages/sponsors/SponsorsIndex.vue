4df4e14d088629f470ec42c05431142282c79747
Mingcong Bai <jeffbai@aosc.io>
2025-08-02 16:37:27 +0800

    feat(sponsors): 添加 AOSCC 2025 捐赠者

--- src/pages/sponsors/SponsorsIndex.vue
+++ src/pages/sponsors/SponsorsIndex.vue
@@ -292,6 +292,58 @@ const sponsorsListHistoric = ref([
 
       <table>
         <caption class="text-[12pt] font-semibold">
+          AOSCC 2025 会务款项（2025 年 7 月 19 日）
+        </caption>
+        <tbody>
+          <tr>
+            <td>Alpine 中文小群</td>
+            <td>baka</td>
+            <td>Camelia Atelier@CLS</td>
+            <td>ChiyoYuki</td>
+          </tr>
+          <tr>
+            <td>冲浪的小鱼</td>
+            <td>Christine Gu</td>
+            <td>Deng</td>
+            <td>邓子 (Deng Yichao)</td>
+          </tr>
+          <tr>
+            <td>Frank@xdlinux</td>
+            <td>禾风之水</td>
+            <td>imgradeone</td>
+            <td>KF</td>
+          </tr>
+          <tr>
+            <td>林子</td>
+            <td>lz差不多是条咸鱼了</td>
+            <td>猫叔</td>
+            <td>喵酱Catty</td>
+          </tr>
+          <tr>
+            <td>Mikura Kyouka</td>
+            <td>内蒙古合诚网络科技有限公司</td>
+            <td>内蒙古墨云网络科技有限公司</td>
+            <td>匿</td>
+          </tr>
+          <tr>
+            <td>nobootleg</td>
+            <td>曲阜师范大学计算机爱好者协会</td>
+            <td>蚊子 (Mozz.ie)</td>
+            <td>御坂云见#17535</td>
+          </tr>
+          <tr>
+            <td>自言者一么</td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br />
+
+      <table>
+        <caption class="text-[12pt] font-semibold">
           软件源源服务器冷存储用硬盘（2025 年 2 月 6 日）
         </caption>
         <tbody>
