40f8bd44c914c3564537d125fbcae83a533f1abf
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-08-11 11:34:30 +0800

    fix: strange anchor behavior in Chrome

--- app/router.options.ts
+++ app/router.options.ts
@@ -4,6 +4,6 @@ import { useScrollStore } from '~/stores/scroll';
 export default <RouterConfig>{
   scrollBehavior(to, _from, savedPosition) {
     if (savedPosition) return savedPosition;
-    if (to.hash) useScrollStore().scrollOrSet(to.hash);
+    else if (to.hash) useScrollStore().scrollOrSet(to.hash);
   }
 };
