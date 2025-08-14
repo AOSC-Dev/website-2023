83089ba88091d52c7342054e015fcda20178fb14
hrh <hrhdeyouxiang@qq.com>
2025-08-14 19:00:13 +0800

    fix: 修复侧栏自动展开 bug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="min-h-[100vh] min-w-[960px]">
-    <div class="my-0 flex min-h-[100vh] flex-col p-0">
+    <div class="header_main my-0 flex flex-col p-0">
       <BarHeader />
-      <div class="main-content flex flex-1 justify-center pb-[2.5rem]">
+      <div class="main-content flex flex-1 justify-center pb-[0.5rem]">
         <div class="content-container myShadow flex">
           <div
             class="sticky top-0 w-[15%] min-w-[12rem] bg-leftbar-bg *:text-nowrap">
@@ -15,3 +15,8 @@
     <BarFooter class="fixed bottom-0 h-[2rem]" />
   </div>
 </template>
+<style lang="scss">
+.header_main {
+  height: calc(100vh - 2rem);
+}
+</style>
