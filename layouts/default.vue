e9da389d925956c10d0431f4df10a8642bb1a15a
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2025-08-17 18:09:21 +0800

    fix(layout): restore bottom padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,16 @@
 <template>
-  <div class="min-h-[100vh] min-w-[960px]">
-    <div class="header_main my-0 flex flex-col p-0">
+  <div>
+    <div
+      class="mb-[2.5rem] flex min-h-[calc(100vh-2.5rem)] min-w-[960px] flex-col">
       <BarHeader />
-      <div class="main-content flex flex-1 justify-center pb-[0.5rem]">
-        <div class="content-container myShadow flex">
-          <div
-            class="sticky top-0 w-[15%] min-w-[12rem] bg-leftbar-bg *:text-nowrap">
-            <BarLeft class="sticky top-0 min-w-[100%] *:text-nowrap" />
-          </div>
-          <slot />
+      <div class="content-container myShadow mx-auto flex flex-grow">
+        <div
+          class="sticky top-0 w-[15%] min-w-[12rem] bg-leftbar-bg *:text-nowrap">
+          <BarLeft class="sticky top-0 min-w-[100%] *:text-nowrap" />
         </div>
+        <slot />
       </div>
     </div>
-    <BarFooter class="fixed bottom-0 h-[2rem]" />
+    <BarFooter class="fixed bottom-0 h-[2rem] min-w-[960px]" />
   </div>
 </template>
-<style lang="scss">
-.header_main {
-  height: calc(100vh - 2rem);
-}
-</style>
