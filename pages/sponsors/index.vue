67d5dae114184636558325a08806f30b6bed94bc
hrh <hrhdeyouxiang@qq.com>
2025-08-21 15:45:05 +0800

    feat: 赞助方/个人赞助者栏表格实现精简

--- pages/sponsors/index.vue
+++ pages/sponsors/index.vue
@@ -78,75 +78,13 @@ const sponsorList = [
           :key="description">
           {{ description }}
         </p>
-        <table>
-          <caption class="text-[12pt] font-semibold">
-            AOSCC 2025 会务款项（2025 年 7 月 19 日）
-          </caption>
-          <tbody>
-            <tr>
-              <td>Alpine 中文小群</td>
-              <td>baka</td>
-              <td>Camelia Atelier@CLS</td>
-              <td>ChiyoYuki</td>
-            </tr>
-            <tr>
-              <td>冲浪的小鱼</td>
-              <td>Christine Gu</td>
-              <td>Deng</td>
-              <td>邓子 (Deng Yichao)</td>
-            </tr>
-            <tr>
-              <td>Frank@xdlinux</td>
-              <td>禾风之水</td>
-              <td>imgradeone</td>
-              <td>KF</td>
-            </tr>
-            <tr>
-              <td>林子</td>
-              <td>lz差不多是条咸鱼了</td>
-              <td>猫叔</td>
-              <td>喵酱Catty</td>
-            </tr>
-            <tr>
-              <td>Mikura Kyouka</td>
-              <td>内蒙古合诚网络科技有限公司</td>
-              <td>内蒙古墨云网络科技有限公司</td>
-              <td>匿</td>
-            </tr>
-            <tr>
-              <td>nobootleg</td>
-              <td>曲阜师范大学计算机爱好者协会</td>
-              <td>蚊子 (Mozz.ie)</td>
-              <td>御坂云见#17535</td>
-            </tr>
-            <tr>
-              <td>自言者一么</td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+        <AppTableOrdinary
+          :table-value="textValue.individualDonors.tableAoscc2025"
+          my-key="sponsors-index-1" />
         <br />
-        <table>
-          <caption class="text-[12pt] font-semibold">
-            软件源源服务器冷存储用硬盘（2025 年 2 月 6 日）
-          </caption>
-          <tbody>
-            <tr>
-              <td>冲浪的小鱼</td>
-              <td>黄逸君</td>
-              <td>kf</td>
-              <td>刘璨贤</td>
-            </tr>
-            <tr>
-              <td>okami2012</td>
-              <td>要塞姬Ilikara</td>
-              <td>Yo Kaslana</td>
-              <td>梓瑶</td>
-            </tr>
-          </tbody>
-        </table>
+        <AppTableOrdinary
+          :table-value="textValue.individualDonors.tableServerHardDrive"
+          my-key="sponsors-index-1" />
       </div>
     </div>
   </div>
